4721c45a962f4b6b59ed720f92d38fbc387f0911
Ahmed.Dakrory <Ahmed.Dakrory@10.50.5.28>
2019-07-28 11:08:28 +0200

    change live ids

--- src/main/com/zc/tools/Constants.java
+++ src/main/com/zc/tools/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
 	public static String api_key="ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SndjbTltYVd4bFgzQnJJam96TVRreExDSnVZVzFsSWpvaWFXNXBkR2xoYkNJc0ltTnNZWE56SWpvaVRXVnlZMmhoYm5RaWZRLnlNbEhrRW9CZjhMZGpUM3pvTF9IVWtRbTdLU3lMeHFyY3ZacWx1T1BzdW1lVlpiWGZQdlBMeUliUXRqdTJfZnZhemNWcVhxS3pOUFBibHF6OFBXNG5B";
 	public static String WEACCEPT_URL="https://accept.paymobsolutions.com/api/";
 	public static Integer EXPIRATION_TIME=3600;
-	public static Integer INTEGRATION_ID_OnlineMethod=4882;
+	public static Integer INTEGRATION_ID_OnlineMethod=5939;
 	public static Integer INTEGRATION_ID_Wallet_Method=4883;
-	public static Integer INTEGRATION_ID_Kiosk_Method=4884;
+	public static Integer INTEGRATION_ID_Kiosk_Method=5938;
 
 	public static Integer MethodOnline=1;
 	public static Integer MethodKiosk=2;
